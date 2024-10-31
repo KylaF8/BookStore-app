73ddb857bda8c74cd01a8cd19bc30370ee33e309
KylaF8 <20098155@mail.wit.ie>
2024-10-31 12:21:42 +0000

    Initial commit

---
+++ test/bookstore-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BookstoreApp from '../lib/bookstore-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bookstore-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BookstoreApp.BookstoreAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
