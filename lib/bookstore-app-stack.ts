d7618a8b337c57a839064ba48524d0225f056564
KylaF8 <20098155@mail.wit.ie>
2024-10-31 13:11:01 +0000

    Some lambda functioning

--- lib/bookstore-app-stack.ts
+++ lib/bookstore-app-stack.ts
@@ -1,16 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BookstoreAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bookstoreFn = new lambdanode.NodejsFunction(this, "BookStoreFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: `${__dirname}/../lambdas/bookstore.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BookstoreAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
